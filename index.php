67325683896b6abd4ce242b673cfbcc4f8e53201
thaivm-0609 <vuongthai95@gmail.com>
2024-09-18 12:06:30 +0700

    feat: init class, object

--- index.php
+++ index.php
@@ -1,13 +1,30 @@
 <?php 
 //khai báo/khởi tạo 1 lớp/class
-class sinhVien {
-    //thuộc tính
+class SinhVien {
+    /** 4 tính chất của Lập trình hướng đối tượng OOP:
+     *  - Tính đóng gói:
+     *  - Tính trừu tượng:
+     *  - Tính kế thừa: class con kế thừa class cha sẽ có thể sử dụng những thuộc tính/phương thức (public/protected) mà class cha đã định nghĩa 
+     *  - Tính đa hình: 
+     */
+
+    /** Access level: phạm vi truy cập, thể hiện tính đóng gói
+     * - public: có thể truy cập từ bất cứ đâu
+     * - protected: chỉ bản thân class + class con có thể truy cập
+     * - private: chỉ bản thân class được truy cập
+     */
+
+    //thuộc tính: $bien
     public $name;
     public $code;
     public $email;
 
-    //phương thức
-    //hàm construct được thực thi ngay khi khởi tạo class;
+    //phương thức: function
+    /** hàm __construct được thực thi ngay khi khởi tạo class (constructor)
+     * - Không định nghĩa __construct: PHP sẽ tự động tạo 1 hàm __construct ko có tham số truyền vào
+     * - Có thể truyền tham số
+     *  */ 
+
     public function __construct($ten, $maSV, $thuDienTu)
     {
         //gán lại giá trị $name->$ten, $code->$maSV, $email->$thuDienTu
@@ -16,19 +33,24 @@ class sinhVien {
         $this->email = $thuDienTu;
     }
 
-
     public function hienThiThongTin() {
-        //sử dụng $this-> để truy cập đến các thuộc tính của class
+        //sử dụng $this->tenThuocTinh để truy cập đến các thuộc tính của class
         echo "ten: $this->name <br>";
         echo "code: $this->code <br>";
         echo "email: $this->email";
     }
+
+    //hàm __destruct (destructor)
+    public function __destruct()
+    {
+        
+    }
 }
 
-//khởi tạo object từ class sinhVien: $tenObject = new tenClass();
-$hung = new sinhVien('hung', 'ph12345', 'hung@fpt.edu.vn');
-//truy cập thuộc tính của object
-echo $hung->name;
-//thực thi hàm
-$hung->hienThiThongTin();
+//khởi tạo object từ class sinhVien: $tenObject = new TenClass(tham-số);
+$thaivm2 = new SinhVien('thaivm2', 'PH12345', 'thaivm2@fe.edu.vn');
+//truy cập thuộc tính của object: $tenObject->tenThuocTinh
+echo "$thaivm2->name <br>";
+//thực thi hàm: $tenObject->tenHam()
+$thaivm2->hienThiThongTin();
 ?>
